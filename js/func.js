201ae4259bce4b2b388ee603afd06662176b4d9d
Reza <reza.jelodar@gmail.com>
2019-03-24 13:55:44 +0100

    migrating to static version

---
+++ js/func.js
@@ -0,0 +1,9 @@
+function isValidateEmail(email) {
+    if(!email)
+        return false;
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
+
+
